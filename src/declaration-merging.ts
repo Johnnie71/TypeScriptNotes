30c613f8849da2d1398eb05e7fb2e27266d43fe8
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-27 15:09:12 -0500

    merging namespaces

--- src/declaration-merging.ts
+++ src/declaration-merging.ts
@@ -23,3 +23,29 @@ let myCart: Cart = {
 		return 1;
 	},
 };
+
+/*
+ Merging namespaces
+ */
+
+namespace MyNamespace {
+	export const x: number = 10;
+	export interface SomeInterface {
+		y: number;
+	}
+}
+
+namespace MyNamespace {
+	export const getX = () => x;
+	export interface SomeInterface {
+		x: number;
+	}
+}
+
+MyNamespace.x;
+MyNamespace.getX();
+
+const someInterface: MyNamespace.SomeInterface = {
+	x: 1,
+	y: 2,
+};
