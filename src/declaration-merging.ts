5c426e0f1fa479fd07940bc2fd44ab5f2e150115
johnnie71 <johnnie.agonz71@gmail.com>
2021-12-27 15:25:47 -0500

    Using namespace to add static members to enums

--- src/declaration-merging.ts
+++ src/declaration-merging.ts
@@ -63,3 +63,20 @@ namespace someFunction {
 }
 
 someFunction.someProperty;
+
+/**
+ Using namespace to add static members to enums
+ */
+
+enum Vegetables {
+	Tomato = "tomato",
+	Onion = "onion",
+}
+
+namespace Vegetables {
+	export function makeSalad() {
+		return Vegetables.Tomato + Vegetables.Onion;
+	}
+}
+
+const salad = Vegetables.makeSalad();
